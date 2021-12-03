78a215d8d8d26ef8f85f3350780ec79aedf37f36
youseefmoemen <youseefmoemen293@gmail.com>
2021-12-04 01:20:39 +0200

    changes

--- Router.java
+++ Router.java
@@ -1,27 +1,35 @@
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Vector;
 import java.util.concurrent.TimeUnit;
+
 
 public class Router{
     int size;
-    ArrayList<Device> devices;
+    Device devices[];
     Semaphore connected;
     Semaphore valid;
-    FileWriter out;
     Router(int init){
         size = init;
-        devices = new ArrayList<Device>(size);
+        devices = new Device[size];
+        for (int i = 0; i < size; i++){
+            devices[i] = new Device("-1", "-1", this);
+        }
         valid = new Semaphore(size);
         connected = new Semaphore(0);
     }
     public void connect(Device d) {
         valid.P(d, true);
-        devices.add(d);
+        int hold = -1;
+        for(int i = 0; i < size; i++){
+            if(devices[i].name.equals("-1")){
+                devices[i] =  d;
+                hold = i;
+                break;
+            }
+        }
         try {
             FileWriter out = new FileWriter("out.text", true);
-            out.write("Connection " + devices.indexOf(d) + ": " + d.name + " Occupied\n");
+            out.write("Connection " + hold + ": " + d.name + " Occupied\n");
             out.close();
         } catch (IOException e) {
             e.printStackTrace();
@@ -30,26 +38,39 @@ public class Router{
     }
     public void disconnect(Device d) {
         connected.P(d, false);
+        int hold = -1;
+        for(int i = 0; i < size; i++){
+            if(devices[i].equals(d)){
+                devices[i] = new Device("-1", "-1", this);
+                hold = i;
+                break;
+            }
+        }
         try {
             FileWriter out = new FileWriter("out.text", true);
-            out.write("Connection "+ devices.indexOf(d) + ": " + d.name + " logged out\n");
+            out.write("Connection "+ hold + ": " + d.name + " logged out\n");
             out.close();
         } catch (IOException e) {
             e.printStackTrace();
-        }       
-        devices.remove(d);
+        }
         valid.V();
     }
     public void action(int t, Device d){
         try {
             try {
                 FileWriter out = new FileWriter("out.text", true);
-                out.write("Connection " + devices.indexOf(d) + ": " + d.name + " perform online action takes " + t + " seconds\n");
+                int hold = -1;
+                for(int i = 0; i < size; i++){
+                    if(devices[i].equals(d)){
+                        hold = i;
+                        break;
+                    }
+                }
+                out.write("Connection " + hold + ": " + d.name + " perform online action takes " + t + " seconds\n");
                 out.close();
             } catch (IOException e) {
                 e.printStackTrace();
-            }
-            
+            }            
             TimeUnit.SECONDS.sleep(t);
         } catch (InterruptedException e) {
             e.printStackTrace();
